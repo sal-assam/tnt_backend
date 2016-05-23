ed6e81f4c414d86c10bbde4535bf76079f2735a9
Tom <thomas.grujic@gmail.com>
2016-05-23 16:59:09 +0100

    test change

--- shellscripts/runtnt.sh
+++ shellscripts/runtnt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Submit the job
-# qsub -N tntweb_${1} -j n -S /bin/bash -cwd -e errors/${1}.err -o logs/${1}.log -m n jobtnt.sh $*
+# test qsub -N tntweb_${1} -j n -S /bin/bash -cwd -e errors/${1}.err -o logs/${1}.log -m n jobtnt.sh $*
 
 LOGFNAME=/home/tntweb/tnt_backend/shellscripts/logs/${1}.log
 
